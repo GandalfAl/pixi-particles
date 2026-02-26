bb7cc550ab2517240935c0f4238b9434ae6794d8
GandalfAl <gandalf00077@gmail.com>
2026-02-26 17:29:20 +0100

    Simple example

--- pixi-sandbox/src/main-scene.ts
+++ pixi-sandbox/src/main-scene.ts
@@ -1,26 +1,42 @@
-import { Assets, Container, Graphics, Sprite, Texture } from "pixi.js";
+import { Container, Graphics } from "pixi.js";
 import { Scene } from "./scene";
-import { AlphaBehaviour, Emitter, SingleTextureProvider } from "pixi-particles-engine";
+import {
+    AlphaBehaviour,
+    Emitter,
+    RadialBurstBehaviour,
+    RectangleSpawnBehaviour,
+    ScaleCurveBehaviour,
+    SingleTextureProvider,
+    StaticRotationBehaviour,
+} from "pixi-particles-engine";
 
 export class MainScene extends Container implements Scene {
     constructor() {
         super({ label: "MainScene" });
 
-        const graphicsTest = new Graphics();
-        graphicsTest.rect(0, 0, 100, 100).fill("red");
-        this.addChild(graphicsTest);
-
         const emitter = new Emitter(
             {
                 lifetime: { min: 1, max: 2 },
                 mode: "wave",
-                ratePerSecond: 1,
-                maxParticles: 1,
-                waveInterval: 1,
-                particlesPerWave: 1,
-                containerOptions: { x: 400, y: 300 },
+                ratePerSecond: 0.1,
+                maxParticles: 400,
+                waveInterval: 0.05,
+                particlesPerWave: 4,
+                containerOptions: { label: "ExampleEmitter", blendMode: "screen", x: 400, y: 300 },
                 emitting: true,
-                behaviours: [new AlphaBehaviour(1, 0)],
+                behaviours: [
+                    new RadialBurstBehaviour(200, 250),
+                    new AlphaBehaviour(1, 0),
+                    new StaticRotationBehaviour({ min: -4, max: 4 }),
+                    new ScaleCurveBehaviour([
+                        { time: 0, value: 0.4 },
+                        { time: 0.1, value: 1.1 },
+                        { time: 0.5, value: 0 },
+                        { time: 0.8, value: 1 },
+                        { time: 0.82, value: 1.2 },
+                    ]),
+                ],
+                addAtBack: true,
             },
             new SingleTextureProvider("Sparkle"),
         );
