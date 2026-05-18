71bfba849acdddbe92f0812b00c8a5cb44fce1a9
GandalfAl <gandalf00077@gmail.com>
2026-05-18 21:13:15 +0200

    Testing behaviours

--- pixi-sandbox/webpack.config.js
+++ pixi-sandbox/webpack.config.js
@@ -48,7 +48,7 @@ module.exports = {
         static: {
             directory: path.join(__dirname, "dist"),
         },
-        port: 3000,
+        port: 3001,
         open: true,
         hot: true,
         watchFiles: ["assets/**/*"], // optional, ensures full watch coverage
